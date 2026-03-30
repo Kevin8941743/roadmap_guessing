3d8c267fda13305b6050b758db446dded7833d85
Kevin thapa <bobmarley9461238@gmail.com>
2026-03-30 12:30:42 +0100

    Initialising variablesfor counting game and random value between 1-100

--- guessing.js
+++ guessing.js
@@ -5,3 +5,12 @@ const rl = readline.createInterface({
     output: process.stdout,
 })
 
+const args = process.argv
+let option = Number(args[2])
+let random_value = Math.floor(Math.random() * 101)
+console.log(random_value)
+let difficulty = ""
+let count = 0
+let choice = 0
+
+
